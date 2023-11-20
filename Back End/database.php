8521b8e002df6ed115bf7c944c8b2173888aedf5
jeremy.julian <jeremy.julian@binus.edu>
2023-11-20 14:37:00 +0700

    Fix login page connect db

--- Back End/database.php
+++ Back End/database.php
@@ -1,14 +1,23 @@
 <?php
 
-    $db_host = "127.0.0.1";
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "connectthread";
-    
-    $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
-    
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+$config = [
+    'server' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'connectthread',
+];
+
+$db = new mysqli(
+    $config["server"],
+    $config["username"],
+    $config["password"],
+    $config["database"]
+);
+
+echo("<script>console.log('PHP: " . json_encode($db) . "');</script>");
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
 
 ?>
--- Front End/login.php
+++ Front End/login.php
@@ -2,6 +2,8 @@
 
 include "../Back End/database.php";
 
+global $db;
+
 if (isset($_SESSION['auth']) && $_SESSION['auth'] === 1) {
     header("Location: index.php");
     exit();
@@ -10,21 +12,43 @@ if (isset($_SESSION['auth']) && $_SESSION['auth'] === 1) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
+    echo("<script>console.log('PHP: " . json_encode($username) . "');</script>");
+    echo("<script>console.log('PHP: " . json_encode($password) . "');</script>");
+
 
     // Modify this part to use MySQL for authentication
-    $sql = "SELECT * FROM users WHERE username = '$username'";
-    $result = mysqli_query($conn, $sql);
+    $query = "SELECT * FROM users WHERE username=? AND password=?;";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $db->close();
+    $row=$result->fetch_assoc();
+    echo("<script>console.log('PHP: " . json_encode($row) . "');</script>");
 
-    if (mysqli_num_rows($result) == 1) {
-        $row = mysqli_fetch_assoc($result);
-        if (password_verify($password, $row['password'])) {
-            $_SESSION["auth"] = 1;
-            header("Location: success.php"); // Redirect to success page
-            exit();
-        }
+    if ($row) {
+        $_SESSION["auth"] = 1;
+        header("Location: success.php"); // Redirect to success page
+    } else {
+        header("Location: error.php"); // Redirect to error page
+        exit();
     }
-    header("Location: error.php"); // Redirect to error page
-    exit();
+
+
+//    $sql = "SELECT * FROM users WHERE username = '$username'";
+//    $result = mysqli_query($db, $sql);
+//    echo("<script>console.log('PHP: ".json_encode($db)."');</script>");
+//
+//    if (mysqli_num_rows($result) == 1) {
+//        $row = mysqli_fetch_assoc($result);
+//        if (password_verify($password, $row['password'])) {
+//            $_SESSION["auth"] = 1;
+//            header("Location: success.php"); // Redirect to success page
+//            exit();
+//        }
+//    }
+//    header("Location: error.php"); // Redirect to error page
+//    exit();
 }
 ?>
 
@@ -35,22 +59,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>ConnectThread</title>
 </head>
 <body>
-    <form class="login-page" action="login.php" method="post">
-        <h2>Welcome to ConnectThread!</h2>
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" placeholder="Enter your username" id="username" name="username" required>
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" placeholder="Enter your password" id="password" name="password" required>
-        </div>
-        <div>
-            <input type="submit" value="Login">
-        </div>
-        <div class="register-link">
-            <p>Haven't registered yet? <a href="registration.php">Register now</a></p>
-        </div>
-    </form>
+<form class="login-page" action="login.php" method="post">
+    <h2>Welcome to ConnectThread!</h2>
+    <div>
+        <label for="username">Username:</label>
+        <input type="text" placeholder="Enter your username" id="username" name="username" required>
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" placeholder="Enter your password" id="password" name="password" required>
+    </div>
+    <div>
+        <input type="submit" value="Login">
+    </div>
+    <div class="register-link">
+        <p>Haven't registered yet? <a href="registration.php">Register now</a></p>
+    </div>
+</form>
 </body>
 </html>
