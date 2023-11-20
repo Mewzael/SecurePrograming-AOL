4777e22442defbb14243072b6e275764a13fc16d
jeremy.julian <jeremy.julian@binus.edu>
2023-11-20 15:03:39 +0700

    fix login and register

--- Front End/login.php
+++ Front End/login.php
@@ -13,47 +13,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-
-    // Modify this part to use MySQL for authentication
-    $query = "SELECT * FROM users WHERE username=? AND password=?;";
+    $query = "SELECT * FROM users WHERE username=?;";
     $stmt = $db->prepare($query);
-    $stmt->bind_param("ss", $username, $password);
+    $stmt->bind_param("s", $username);
     $stmt->execute();
     $result = $stmt->get_result();
     $db->close();
-    $row=$result->fetch_assoc();
-//    echo("<script>console.log('PHP: " . json_encode($row) . "');</script>");
+    $row = $result->fetch_assoc();
+    echo("<script>console.log('PHP: " . json_encode($row['password']) . "');</script>");
 
-    if ($row) {
+    if ($row && password_verify($password, $row["password"])) {
         $_SESSION["auth"] = 1;
         header("Location: success.php"); // Redirect to success page
     } else {
         header("Location: error.php"); // Redirect to error page
         exit();
     }
-
-
-//    $sql = "SELECT * FROM users WHERE username = '$username'";
-//    $result = mysqli_query($db, $sql);
-//    echo("<script>console.log('PHP: ".json_encode($db)."');</script>");
-//
-//    if (mysqli_num_rows($result) == 1) {
-//        $row = mysqli_fetch_assoc($result);
-//        if (password_verify($password, $row['password'])) {
-//            $_SESSION["auth"] = 1;
-//            header("Location: success.php"); // Redirect to success page
-//            exit();
-//        }
-//    }
-//    header("Location: error.php"); // Redirect to error page
-//    exit();
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="./static/login.css">
     <title>ConnectThread</title>
 </head>
 <body>
--- Front End/registration.php
+++ Front End/registration.php
@@ -2,80 +2,86 @@
 
 include "../Back End/database.php";
 
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$confirmPassword = $_POST['confirm-password'];
+global $db;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm-password'];
 
 // Perform input validation
-if (empty($username) || empty($email) || empty($password) || empty($confirmPassword)) {
-    header("Location: registration.php?error=empty_fields");
-    exit();
-}
+    if (empty($username) || empty($email) || empty($password) || empty($confirmPassword)) {
+        header("Location: registration.php?error=empty_fields");
+        exit();
+    }
 
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    header("Location: registration.php?error=invalid_email");
-    exit();
-}
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: registration.php?error=invalid_email");
+        exit();
+    }
 
-if ($password != $confirmPassword) {
-    header("Location: registration.php?error=password_mismatch");
-    exit();
-}
+    if ($password != $confirmPassword) {
+        header("Location: registration.php?error=password_mismatch");
+        exit();
+    }
 
 // Hash the password (for security)
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
 // Insert user data into the database using prepared statements
-$sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $username, $email, $hashedPassword);
+    $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("sss", $username, $email, $hashedPassword);
 
-if ($stmt->execute()) {
-    header("Location: registration.php?success=registered");
-} else {
-    $error_message = "Database error: " . $stmt->error;
+    if ($stmt->execute()) {
+        header("Location: registration.php?success=registered");
+    } else {
+        $error_message = "Database error: " . $stmt->error;
 
-    error_log($error_message, 0);
+        error_log($error_message, 0);
 
-    header("Location: registration.php?error=registration_failed&message=" . urlencode($error_message));
+        header("Location: registration.php?error=registration_failed&message=" . urlencode($error_message));
+    }
+
+    $stmt->close();
+    $db->close();
 }
-
-$stmt->close();
-$conn->close();
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="/static/register.css">
+    <link rel="stylesheet" type="text/css" href="./static/register.css">
     <title>ConnectThread - Register</title>
 </head>
 <body>
-    <form class="register-page" action="registration.php" method="post">
-        <h2>Register for ConnectThread</h2>
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="Enter a username" required>
-        </div>
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" required>
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Create a password" required>
-        </div>
-        <div>
-            <label for="confirm-password">Confirm Password:</label>
-            <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password" required>
-        </div>
-        <div>
-            <input type="submit" value="Register">
-        </div>
-        <div class="login-link">
-            <p>Already have an account? <a href="login.php">Log in</a></p>
-        </div>
-    </form>
+<form class="register-page" action="registration.php" method="post">
+    <h2>Register for ConnectThread</h2>
+    <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" placeholder="Enter a username" required>
+    </div>
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder="Enter your email" required>
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder="Create a password" required>
+    </div>
+    <div>
+        <label for="confirm-password">Confirm Password:</label>
+        <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password"
+               required>
+    </div>
+    <div>
+        <input type="submit" value="Register">
+    </div>
+    <div class="login-link">
+        <p>Already have an account? <a href="login.php">Log in</a></p>
+    </div>
+</form>
 </body>
 </html>
